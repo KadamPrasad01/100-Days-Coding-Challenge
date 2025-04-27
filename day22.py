f7f65fa4f3b21a5aa12749a98c342bcde8f9859d
KadamPrasad01 <kadamprasad1015@gmail.com>
2025-04-27 19:22:36 +0530

    Create day22.py

---
+++ day22.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        prev = None
+        temp = head
+
+        while temp:
+            next_node = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next_node
+        return prev
