c49c5d7053adeb0b07e14451b0254378c9839044
KadamPrasad01 <kadamprasad1015@gmail.com>
2025-05-09 16:10:31 +0530

    Create day34.py

---
+++ day34.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        count = 0
+        for jewel in jewels:
+            for stone in stones:
+                if(jewel == stone):
+                    count+=1
+        return count
